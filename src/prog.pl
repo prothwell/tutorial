a28d531bd5a1f2e5467ccecdf8de22a5b251a4dc
Paul Rothwell <Paul.Rothwell@cit.ie>
2017-06-13 13:14:07 +0100

    adding more power to the prototype.

--- src/prog.pl
+++ src/prog.pl
@@ -2,4 +2,5 @@
 
 print "Hello, World...\n";
 print 10**5, "\n"; #NO! should be 5 you plonker - sheeze!
+print 2**8, "\n",2**8, "\n", 2**8, "\n"; #more powers to me for the prototype
 print "I may well have forgotten how to program in Perl.\n";
