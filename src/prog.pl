7e57771a84c950429bbb9acf6ede06a4d7738192
prothwell <paul.rothwell@cit.ie>
2017-06-13 12:48:08 +0100

    I am unhappy with your sloppy code rothwell

--- src/prog.pl
+++ src/prog.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
 
 print "Hello, World...\n";
-print 10**3, "\n";
+print 10**4, "\n"; #I think it should be to the fourth power
 print "I may well have forgotten how to program in Perl.\n";
