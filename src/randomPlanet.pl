0ed5be277447ebdf3b8f2a4aa3dbe4f7681a95bd
Paul Rothwell <Paul.Rothwell@cit.ie>
2017-06-13 15:39:31 +0100

    added random planet script

---
+++ src/randomPlanet.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+print "Hello, World...\n";
+my @planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune");
+my $p=int(rand(8));
+print "A random planet is $planets[$p].\n";
+
